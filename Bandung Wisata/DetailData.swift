eb977467c7d92c6b22fbddc89439992df622c867
steev-tupanwael <steevtupanwael@gmail.com>
2016-01-26 14:45:48 +0700

    json data done

---
+++ Bandung Wisata/DetailData.swift
@@ -0,0 +1,19 @@
+//
+//  DetailData.swift
+//  Bandung Wisata
+//
+//  Created by Steev Tupanwael on 1/26/16.
+//  Copyright © 2016 Kota Bandung. All rights reserved.
+//
+
+import Foundation
+
+struct DetailData {
+    var detjudul : String
+    var detsubjudul : String
+    var detalamat : String
+    var detkontak : String
+    var detdeskripsi : String
+    var detgambar : String
+    var detlokasikoor : String
+}
--- Bandung Wisata/MasterViewController.swift
+++ Bandung Wisata/MasterViewController.swift
@@ -11,20 +11,46 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
-
+    var detailArray = [DetailData?]()
+    var numberofRows = 0
+    var dataIdArray = [String]()
+    var dataJudulArray = [String]()
+    var dataSubjudulArray = [String]()
+    var dataAlamatArray = [String]()
+    var dataKontakArray = [String]()
+    var dataDeskripsiArray = [String]()
+    var dataGambarArray = [String]()
+    var dataLokasikoorArray = [String]()
+    
+    func parseJSON() {
+        let path :  String = NSBundle.mainBundle().pathForResource("jsonFile", ofType: "json") as String!
+        let jsonData = NSData(contentsOfFile: path) as NSData!
+        let readableJSON = JSON(data: jsonData, options: NSJSONReadingOptions.MutableContainers, error:  nil)
+        
+        numberofRows = readableJSON["data"].count - 1
+        
+        for i in 0...numberofRows {
+            let judul = readableJSON["data"][i]["judul"].string as String!
+            let subjudul = readableJSON["data"][i]["subjudul"].string as String!
+            let alamat = readableJSON["data"][i]["alamat"].string as String!
+            let kontak = readableJSON["data"][i]["kontak"].string as String!
+            let deskripsi = readableJSON["data"][i]["deskripsi"].string as String!
+            let gambar = readableJSON["data"][i]["gambar"].string as String!
+            let lokasikoor = readableJSON["data"][i]["lokasikoor"].string as String!
+            
+            dataJudulArray.append(judul)
+            dataSubjudulArray.append(subjudul)
+            
+            detailArray.append(DetailData(detjudul: judul, detsubjudul: subjudul, detalamat: alamat, detkontak: kontak, detdeskripsi: deskripsi, detgambar: gambar, detlokasikoor: lokasikoor))
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+        
+        parseJSON()
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -37,20 +63,15 @@ class MasterViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                var detailArray2 : DetailData
+                detailArray2 = detailArray[indexPath.row]!
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+                controller.detailItem = detailArray2.detjudul
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
@@ -64,29 +85,20 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return numberofRows
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
-        return cell
-    }
-
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        
+        if dataJudulArray.count != 0 {
+            cell.textLabel!.text = dataJudulArray[indexPath.row]
         }
+        if dataSubjudulArray.count != 0 {
+            cell.detailTextLabel!.text = dataSubjudulArray[indexPath.row]
+        }
+        
+        return cell
     }
 
 
